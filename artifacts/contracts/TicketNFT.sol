3e894ebe7087e3fcf86c2b752304c7fec0771b7a
oumaoumag <ouma.godwin10@gmail.com>
2025-12-18 02:56:25 +0300

    list openzeppelin imports

---
+++ artifacts/contracts/TicketNFT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/proxy/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+* @title TicketNFT
+* @notice ERC1155 implementation for multi-tier event tickets
+* @dev Cloned per event via EventFactory
+*/
